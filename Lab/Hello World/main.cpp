9e67a587c1d38560aae62f1bda1cb39fa1812e61
iceshadoz <c_Vasquez257@yahoo.com>
2015-05-13 10:16:57 -0700

    Signed-off-by: iceshadoz <c_Vasquez257@yahoo.com>

---
+++ Lab/Hello World/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: Christopher Vasquez
+ * Created on May 13, 2015, 8:55 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    cout<<"Hello World"<<endl;
+    return 0;
+}
+
